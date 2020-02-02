0206412b7baf93111be9380b82ee49e730613752
Phillip Jones <ptj92e@gmail.com>
2020-02-01 21:23:58 -0600

    Set up server to utilize sequelize, mysql, and handlebars

---
+++ server.js
@@ -0,0 +1,18 @@
+let express = require("express");
+let app = express();
+let PORT = process.env.PORT || 8080;
+let exphbs = require("express-handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("app/public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./app/routes/api-routes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
